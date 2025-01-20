81b8483002553a3ae4955ffec6729ce81721e0a6
GFGaldeano <gustavo_galdeano@yahoo.com.ar>
2025-01-20 19:35:48 -0300

    entidad helthsheet-planilla de salud

---
+++ src/entities/helthsheet.entity.ts
@@ -0,0 +1,23 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { User } from './user.entity';
+import { ApiProperty } from '@nestjs/swagger';
+
+@Entity('helthsheet')
+export class HealthSheet {
+  @PrimaryGeneratedColumn('uuid')
+  id_sheet: string;
+
+   @ApiProperty({
+      description: 'Clave foránea que referencia al usuario con la planilla de salud',
+      example: '0b8bffdd-a027-41b6-a2c9-f12d8ad1a0ec',
+    })
+  @ManyToOne(() => User, (user) => user.healthSheets)
+  user: User;
+
+  @Column({ length: 100 })
+  @ApiProperty({
+    description: 'URL de Cloudinary donde se encuentra la planilla de salud',
+    example: 'https://res.cloudinary.com/dj0v6zokk',
+  })
+  urlSheet: string;
+}
