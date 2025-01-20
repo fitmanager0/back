c505dd54770315fc434662b177f85dda856cfd06
GFGaldeano <gustavo_galdeano@yahoo.com.ar>
2025-01-20 19:43:14 -0300

    configuracion swagger - main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  const swaggerConfig = new DocumentBuilder()
+  .setTitle('FITMANAGER - PROYECTO FINAL - BACKEND')
+  .setDescription('DOCUMENTACIÓN DE LA API DE FITMANAGER')
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document,{swaggerOptions:{tagsSorter:'alpha'},customSiteTitle: 'PF - FITMANAGER'});
+
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
