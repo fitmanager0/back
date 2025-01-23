511514e5c8f0fe846e562152b28d04254a6c8067
Enrique Castillo <enriquecastiyosoy@gmail.com>
2025-01-23 15:45:34 -0500

    documentacion con ejemplos de rutina y nivel. posibles cambios de entidad y dto por logica, a analizar

--- src/dtos/create-routine.dto.ts
+++ src/dtos/create-routine.dto.ts
@@ -1,15 +1,31 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsUUID, IsString, IsNotEmpty } from 'class-validator';
 
 export class CreateRoutineDto {
-  @IsString()
-  @IsNotEmpty()
+  @ApiProperty({
+    description:
+      'URL de la rutina. Campo de tipo string con un máximo de 100 caracteres.',
+    example: 'https://example.com/routine1',
+  })
+  @IsNotEmpty({ message: 'La URL de la rutina es obligatoria.' })
+  @IsString({ message: 'La URL de la rutina debe ser una cadena de texto.' })
   url_routine: string;
 
-  @IsUUID()
-  @IsNotEmpty()
+  @ApiProperty({
+    description:
+      'Identificador del usuario que tiene la rutina. Debe ser un UUID válido.',
+    example: '550e8400-e29b-41d4-a716-446655440000',
+  })
+  @IsNotEmpty({ message: 'El ID del usuario es obligatorio.' })
+  @IsUUID('4', { message: 'El ID del usuario debe ser un UUID válido.' })
   id_user: string;
 
-  @IsUUID()
-  @IsNotEmpty()
+  @ApiProperty({
+    description:
+      'Identificador del nivel de la rutina. Debe ser un UUID válido.',
+    example: '123e4567-e89b-12d3-a456-426614174000',
+  })
+  @IsNotEmpty({ message: 'El ID del nivel es obligatorio.' })
+  @IsUUID('4', { message: 'El ID del nivel debe ser un UUID válido.' })
   id_level: string;
 }
